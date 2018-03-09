c28b25d6d0a92e8a6585898b31cd188252e29045
adriens <adrien.sales@gmail.com>
2018-03-09 23:01:23 +1100

    Log level cleaning...

--- src/main/java/com/github/adriens/cine/city/noumea/sdk/FilmsWrapper.java
+++ src/main/java/com/github/adriens/cine/city/noumea/sdk/FilmsWrapper.java
@@ -48,9 +48,9 @@ public class FilmsWrapper {
         String tmp = filmURL;
 
         // http://www.cinecity.nc/Cinecity/Film/39428
-        logger.info("Input url to extract : <" + filmURL + ">");
+        logger.debug("Input url to extract : <" + filmURL + ">");
         //tmp = tmp.replace(FilmsWrapper.URL_BASE_FILM8 + "/", "");
-        logger.info("Intermediar transform : <" + tmp + ">");
+        logger.debug("Intermediar transform : <" + tmp + ">");
         return Integer.parseInt(tmp.replace(FilmsWrapper.URL_BASE_FILM8 + "/", ""));
     }
 
@@ -88,13 +88,13 @@ public class FilmsWrapper {
             // cineCity URL
             DomNodeList urls = film.getElementsByTagName("a");
             HtmlElement localUri = (HtmlElement) urls.get(0);
-            logger.info("Cinecity local URL : " + URL_ROOT + localUri.getAttribute("href"));
+            logger.debug("Cinecity local URL : " + URL_ROOT + localUri.getAttribute("href"));
             aFilm.setCinecityFilmURL(new URL(URL_ROOT + localUri.getAttribute("href")));
 
             //filmName
             HtmlElement filmName = (HtmlElement) urls.get(1);
             aFilm.setName(filmName.getTextContent());
-            logger.info("Film name : " + aFilm.getName());
+            logger.debug("Film name : " + aFilm.getName());
 
             // affiche
             HtmlElement affiche = film.getElementsByTagName("img").get(0);
@@ -149,19 +149,19 @@ public class FilmsWrapper {
             filmURL = URL_ROOT + film.getAttribute("href");
 
             aFilm.setCinecityFilmURL(new URL(filmURL));
-            logger.info("Film URL : <" + aFilm.getCinecityFilmURL() + ">");
+            logger.debug("Film URL : <" + aFilm.getCinecityFilmURL() + ">");
 
             //film name
             filmTitle = film.getAttribute("title");
-            logger.info("Film title : <" + filmTitle + ">");
+            logger.debug("Film title : <" + filmTitle + ">");
             aFilm.setName(filmTitle);
             // pas d'affiche sur cette page
             // on va la récuéprer à partir de son URL cinecity
             int lFilmId = FilmsWrapper.extractFilmIdFromFilmURL(aFilm.getCinecityFilmURL().toString());
-            logger.info("Film id : <" + lFilmId + ">");
+            logger.debug("Film id : <" + lFilmId + ">");
             //logger.info("filmId : <");
             FilmsWrapper lWrapper = new FilmsWrapper();
-            logger.info("Affiche URL : <" + lWrapper.getDetailsOfFilm(lFilmId).getAfficheURL() + ">");
+            logger.debug("Affiche URL : <" + lWrapper.getDetailsOfFilm(lFilmId).getAfficheURL() + ">");
             aFilm.setCinecityAfficheURL(lWrapper.getDetailsOfFilm(lFilmId).getAfficheURL());
             out.add(aFilm);
 
@@ -179,11 +179,11 @@ public class FilmsWrapper {
     public static final String cleanRawDetails(String rawDetails) {
         String out;
         out = rawDetails;
-        logger.info("rawdetails : <" + out + ">");
+        logger.debug("rawdetails : <" + out + ">");
         out = out.replace("Voir la bande-annonce", "").trim();
         out = out.substring(0, out.length() - 1);
         out = out.trim();
-        logger.info("cleaned rawdetails : <" + out + ">");
+        logger.debug("cleaned rawdetails : <" + out + ">");
         // Guerre - USA - 2018 - 131 mn - Tous publics
         return out;
     }
@@ -204,32 +204,32 @@ public class FilmsWrapper {
         //http://www.cinecity.nc/Cinecity/Film/39371
         out.setFilmURL(new URL(FilmsWrapper.getFilmURL(filmId)));
         logger.info("Récuperation des details du film <" + filmId + ">");
-        logger.info("Film url : <" + out.getFilmURL() + ">");
+        logger.debug("Film url : <" + out.getFilmURL() + ">");
         // le titre
         String titre = ((HtmlElement) htmlPage.getElementById("fiche_titre")).getTextContent().trim();
-        logger.info("Film details pour <" + filmId + ">");
-        logger.info("Nom du film : <" + titre + ">");
+        logger.debug("Film details pour <" + filmId + ">");
+        logger.debug("Nom du film : <" + titre + ">");
         out.setTitre(titre);
 
         // url de l'affiche
         String afficheURL = ((HtmlElement) htmlPage.getElementById("fiche_affiche")).getElementsByTagName("img").get(0).getAttribute("src");
-        logger.info("detected url : <" + afficheURL + ">");
+        logger.debug("detected url : <" + afficheURL + ">");
         afficheURL = URL_ROOT + afficheURL;
-        logger.info("url complète : <" + afficheURL + ">");
+        logger.debug("url complète : <" + afficheURL + ">");
         out.setAfficheURL(new URL(afficheURL));
         // realisateur
         String realisateur = ((HtmlElement) htmlPage.getElementById("fiche_texte")).getElementsByTagName("p").get(0).getTextContent();
-        logger.info("realisateur : <" + realisateur + ">");
+        logger.debug("realisateur : <" + realisateur + ">");
         out.setRealisateur(realisateur);
 
         //acteurs
         String acteurs = ((HtmlElement) htmlPage.getElementById("fiche_texte")).getElementsByTagName("p").get(1).getTextContent();
-        logger.info("acteurs : <" + acteurs + ">");
+        logger.debug("acteurs : <" + acteurs + ">");
         out.setActeurs(acteurs);
 
         // note
         String note = ((HtmlElement) htmlPage.getElementById("fiche_vote")).getElementsByTagName("span").get(0).getElementsByTagName("b").get(0).getTextContent();
-        logger.info("note : <" + note + ">");
+        logger.debug("note : <" + note + ">");
         out.setNote(Integer.parseInt(note));
 
         // synopsys
@@ -238,14 +238,14 @@ public class FilmsWrapper {
         // le synopsys est toujours le dernier p de la liste
         //synopsys = ((HtmlElement) htmlPage.getElementById("fiche_texte")).getElementsByTagName("p").get(2).getTextContent();
         synopsys = htmlNodes.get(htmlNodes.size()-1).getTextContent();
-        logger.info("Synopsys : <" + synopsys + ">");
+        logger.debug("Synopsys : <" + synopsys + ">");
         out.setSynopsys(synopsys);
 
         // details (Genre - Pays - Année - durée en minutes - Type de public -)
         // exemple : Guerre - USA - 2018 - 131 mn - Tous publics
         // rawDetails
         String rawDetails = ((HtmlElement) htmlPage.getElementById("fiche_texte")).getElementsByTagName("h1").get(0).getTextContent().trim();
-        logger.info("rawDetails : <" + rawDetails + ">");
+        logger.debug("rawDetails : <" + rawDetails + ">");
         out.setRawDetails(FilmsWrapper.cleanRawDetails(rawDetails));
         //String test = FilmsWrapper.cleanRawDetails(rawDetails);
         //FilmsWrapper.getRawDetails(rawDetails);
@@ -253,7 +253,7 @@ public class FilmsWrapper {
         
         // le genre est TOUJOURS en position 0
         out.setGenre(FilmsWrapper.extractGenreFromRawDetails(rawDetails));
-        logger.info("Set genre to <" + out.getGenre() + ">");
+        logger.debug("Set genre to <" + out.getGenre() + ">");
         // Parfois il y a plusieurs pays... ;-(
         //out.setPays(detailsList.get(1));
         
